28a3b51aa713549e6d7cb6e4639076541578b3a0
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2025-09-17 11:49:37 +0530

    ..

---
+++ main.py
@@ -0,0 +1,14 @@
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+def generate_pet_name():
+    llm = OpenAI(temperature=0.7)
+    
+    name = llm("I have a dog pet and I want a cool name for it. Suggest me five cool names for my pet.")
+    
+    return name
+
+if __name__ == "__main__":
+     print(generate_pet_name())
+    
